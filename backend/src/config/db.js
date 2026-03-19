f7a4be9293f4ea9834e69e1b2e44dad873b45ba2
Lautaro Jofre <lautaro.jofre2004@gmail.com>
2026-03-19 14:31:34 -0300

    Configuracion SSL para Neon

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // Esto permite conectar a Neon sin problemas de certificados
+    }
 });
 
 pool.on('connect', () => {
